0e591bc5ca971b24e3beee6396aa8c2cee34e8fd
AmairT <amair0829@gmail.com>
2023-06-12 14:33:13 -0700

    added h2 and ul for age and email

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [puppies, setPuppies] = useState(puppyList)
   const [featPupId, setFeatPupId] = useState(null)
   console.log('puppyList: ', puppyList);
-  const featuredPup = puppies.find((pup) => pup.id===featPupId)
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
   console.log(featuredPup)
   {()=> {console.log('puppy.id: ', puppy.id)}}
   
@@ -16,9 +16,19 @@ function App() {
       <div className="App">
         {
           puppies.map((puppy) => {
-            return <p onClick={()=>{setFeatPupId(puppy.id)}} key = {puppy.id}>{puppy.name}</p>;
+            return <p onClick={()=>{setFeatPupId(puppy.id)}} key = {puppy.id}>{puppy.name}</p>
           })
         }  
+        {featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
+          
       </div>
     </>
   )
