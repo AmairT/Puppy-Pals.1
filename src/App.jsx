0ed449c13233538734d1f2ff54b88132c1ccba46
AmairT <amair0829@gmail.com>
2023-06-11 17:34:23 -0700

    get puppy data with react

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import { useState } from 'react'
+import {puppyList} from './data.js'
 import './App.css'
 
 function App() {
-
-  return
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  console.log(puppies)
+  {()=> {console.log('puppy.id: ', puppy.id)}}
+  return (
     <>
-      <div>
-     
+      <div className="App">
+      {
+        puppies.map((puppy) => {
+          return <p onClick={()=>{}} key= {puppy.id}>{puppy.name}</p>;
+        })        
+      }   
+      </div>
+
+      <div className="card">
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
       </div>
     </>
+  )
 }
+
 export default App
+
