5c84fe16db085d6e2acb54c47edf6bc32ae6fb24
AmairT <amair0829@gmail.com>
2023-06-11 17:42:08 -0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -16,12 +16,6 @@ function App() {
         })        
       }   
       </div>
-
-      <div className="card">
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
     </>
   )
 }
