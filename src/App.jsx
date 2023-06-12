ce8c0e6b2a6b3a265a1755547761c8bc5373b649
AmairT <amair0829@gmail.com>
2023-06-12 12:59:03 -0700

    onClick testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
-import { useState } from 'react'
 import {puppyList} from './data.js'
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [puppies, setPuppies] = useState(puppyList);
-  const [featPupId, setFeatPupId] = useState(null);
-  console.log(puppies)
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+  console.log('puppyList: ', puppyList);
+  const featuredPup = puppies.find((pup) => pup.id===featPupId)
+  console.log(featuredPup)
   {()=> {console.log('puppy.id: ', puppy.id)}}
+  
   return (
     <>
+    <h1>Puppy List</h1>
       <div className="App">
-      {
-        puppies.map((puppy) => {
-          return <p onClick={()=>{}} key= {puppy.id}>{puppy.name}</p>;
-        })        
-      }   
+        {
+          puppies.map((puppy) => {
+            return <p onClick={()=>{setFeatPupId(puppy.id)}} key = {puppy.id}>{puppy.name}</p>;
+          })
+        }  
       </div>
     </>
   )
